2feaf2809a412c19bec068524d74474cdbd0a8ec
Mauricio Rojas <mrojasb2000@gmail.com>
2025-01-29 20:19:56 -0300

    chore: adding create product process and handler exceptions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handlerDBExceptions(error);
+    }
   }
 
   findAll() {
@@ -22,5 +42,12 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handlerDBExceptions(error: any) {
+    this.logger.error(`Error: ${error.code}, detail: ${error.detail}`);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
   }
 }
