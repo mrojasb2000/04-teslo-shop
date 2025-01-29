86a9ca02717dcd86ffd48b8fe8a98fbc7a8dfce0
Mauricio Rojas <mrojasb2000@gmail.com>
2025-01-29 15:25:36 -0300

    chore: adding typeorm and postgres, typeorm config access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: Boolean(process.env.TYPEORM_AUTOLOAD_ENTITIES),
+      synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
