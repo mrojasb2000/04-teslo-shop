6b6d2d4f5b15e1dfeb4d5ece850516dd8b5239e5
Mauricio Rojas <mrojasb2000@gmail.com>
2025-02-18 15:18:02 -0300

    chore: refactor returns product and images once the product is saved

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -37,7 +37,7 @@ export class ProductsService {
         ),
       });
       await this.productRepository.save(product);
-      return product;
+      return { ...product, images };
     } catch (error) {
       this.handlerDBExceptions(error);
     }
