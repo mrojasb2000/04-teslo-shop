918e26ed75e09bcd338e9b15f5a823872b0a5e89
Mauricio Rojas <mrojasb2000@gmail.com>
2025-02-18 11:23:51 -0300

    chore: add product image structure table

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from './entities/product.entity';
+import { ProductImage } from './entities/product-image.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
 })
 export class ProductsModule {}
